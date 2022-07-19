5af44eb1491fa9079c41805fb29574929e271ff2
4lDO2 <4lDO2@protonmail.com>
2022-07-19 10:33:14 +0200

    Use redox-daemon

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,31 @@
-extern crate syscall;
-
-use syscall::data::Packet;
-use syscall::flag::CloneFlags;
-use syscall::scheme::Scheme;
 use std::fs::File;
 use std::io::{Read, Write};
+
+use syscall::data::Packet;
+use syscall::scheme::Scheme;
+
 use scheme::NullScheme;
 
 mod scheme;
 
 fn main() {
-    if unsafe { syscall::clone(CloneFlags::empty()).unwrap() } == 0 {
+    redox_daemon::Daemon::new(move |daemon| {
         let mut socket = File::create(":null").expect("nulld: failed to create null scheme");
         let scheme = NullScheme;
 
         syscall::setrens(0, 0).expect("nulld: failed to enter null namespace");
 
+        daemon.ready().expect("nulld: failed to notify parent");
+
         loop {
             let mut packet = Packet::default();
             if socket.read(&mut packet).expect("nulld: failed to read events from null scheme") == 0 {
-                break;
+                std::process::exit(0);
             }
 
             scheme.handle(&mut packet);
 
             socket.write(&packet).expect("nulld: failed to write responses to null scheme");
         }
-    }
+    }).expect("nulld: failed to daemonize");
 }
